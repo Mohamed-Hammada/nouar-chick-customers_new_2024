122d3f3171739f8799b10d51e89ff23cb0cf09db
Mohamed-Hammada <91836396+Mohamed-Hammada@users.noreply.github.com>
2024-01-04 21:53:13 +0200

    sid nav responsive

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, signal } from '@angular/core';
-import {  } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -8,14 +9,32 @@ import { MatIconModule } from '@angular/material/icon';
 import { CustomSideNavComponent } from "./custom-side-nav/custom-side-nav.component";
 import { CommonModule } from '@angular/common';
 @Component({
-    selector: 'app-navigation',
-    standalone: true,
-    templateUrl: './navigation.component.html',
-    styleUrl: './navigation.component.scss',
-    imports: [CommonModule,MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule, RouterOutlet, CustomSideNavComponent]
+  selector: 'app-navigation',
+  standalone: true,
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss',
+  imports: [CommonModule, MatToolbarModule,
+    MatSidenavModule, MatIconModule,
+    MatButtonModule, RouterOutlet,
+    CustomSideNavComponent]
 })
 export class NavigationComponent {
   collapsed = signal(false);
-  sidenaveWidth = computed(() => this.collapsed()? '65px' : '250px');
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.setCollapsedForScreenSize();
+  }
 
+  sidenaveWidth = computed(() => this.collapsed() ? '65px' : '250px');
+
+  private setCollapsedForScreenSize() {
+    this.breakpointObserver.observe([
+      Breakpoints.Handset
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.collapsed.set(true);
+      } else {
+        this.collapsed.set(false);
+      }
+    });
+  }
 }
