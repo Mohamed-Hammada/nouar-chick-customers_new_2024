690cbe2bbccc2d2985eb508064b38f94696eb9cb
Mohamed-Hammada <91836396+Mohamed-Hammada@users.noreply.github.com>
2024-01-11 16:39:28 +0200

    update product

---
+++ src/app/pages/products/product.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Environment } from '../../../environments/environment';
+import { NotificationService } from '../../../app/components/notification.service'; // Adjust the import path
+
+export type Product = {
+  id?: any;
+  name?: string;
+  description?: string;
+  creation_date?: string;
+  modification_date?: string;
+}
+
+export interface ProductPage {
+  content?: Product[];
+  totalElements?: number;
+  totalPages?: number;
+  pageable?: {
+    page_number?: number;
+    page_size?: number;
+    sort?: {
+      empty?: boolean;
+      sorted?: boolean;
+      unsorted?: boolean;
+    };
+    offset?: number;
+    paged?: boolean;
+    unpaged?: boolean;
+  };
+  last?: boolean;
+  number_of_elements?: number;
+  size?: number;
+  number?: number;
+  sort?: {
+    empty?: boolean;
+    sorted?: boolean;
+    unsorted?: boolean;
+  };
+  empty?: boolean;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  
+  apiUrl: string = `${Environment.apiUrl}/api/products`;
+
+  constructor(private http: HttpClient,private notificationService: NotificationService) { }
+
+
+  searchProducts(term = '', page = 1, size = 10): Observable<ProductPage> {
+    return this.http.get<ProductPage>(`${this.apiUrl}/search`, {
+      params: { term, page: (page - 1).toString(), size: size.toString() }
+    }).pipe(
+      catchError((error: HttpErrorResponse) => this.handleError(error))
+    );
+  }
+
+  getProducts(page = 1, size = 10): Observable<ProductPage> {
+    return this.http.get<ProductPage>(`${this.apiUrl}`, {
+      params: { page: (page - 1).toString(), size: size.toString() }
+    });
+  }
+
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${this.apiUrl}`, product);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${product.id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    // Log the error for debugging purposes
+    console.error('An error occurred:', error);
+
+    // Notify the user with a snackbar message
+    this.notificationService.warn('Something went wrong. Please try again later.');
+
+    // You can return a custom error message or throw the original error
+    return throwError('Something went wrong. Please try again later.');
+  }
+}
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Signal, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Product, ProductPage, ProductService } from './product.service';
+import { Observable } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatIconModule, MatCardModule, MatToolbarModule, TranslateModule, MatButtonModule,HttpClientModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  cards$ : Observable< ProductPage>;
 
+  constructor(private service : ProductService  , private translateService: TranslateService) {
+    const userLang =  'ar'
+    const languageCode = userLang.split('-')[0]
+    this.translateService.setDefaultLang(languageCode);
+    this.translateService.use(languageCode);
+    this.cards$ =  this.service.getProducts()
+  }
 }
